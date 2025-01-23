7b2f6f9e568a2171fc14b0cbb297b7ca9f8a69ee
Ajhar21 <2017ajharakanda@gmail.com>
2025-01-23 21:58:51 +0600

    Delete folder

--- temp-folder/variables.java
+++
@@ -1,8 +0,0 @@
-public class variables {
-    public static void main(String x[]){
-        byte a=127;
-        byte b=30;
-        int result=a*b;  //type promotions
-        System.out.print(result);
-    }
-}
