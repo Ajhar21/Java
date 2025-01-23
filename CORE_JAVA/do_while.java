a0c14a1c1def63f7f96b42bc305555d332a6a447
Ajhar21 <2017ajharakanda@gmail.com>
2025-01-23 21:47:25 +0600

    Core java added

---
+++ CORE_JAVA/do_while.java
@@ -0,0 +1,16 @@
+public class do_while {
+    public static void main(String x[]){
+        int i=5;
+
+        while (i<4){
+            System.out.println("Print Value");
+            i++;
+        }
+
+        do
+        {
+            System.out.println("Print Value");
+            i++;
+        }while (i<4);
+    }
+}
