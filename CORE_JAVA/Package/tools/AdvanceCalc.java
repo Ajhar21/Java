6682263bf8cde75a58e300dbc2307d9d251071eb
Ajhar21 <2017ajharakanda@gmail.com>
2025-06-26 19:55:56 +0600

    added package

---
+++ CORE_JAVA/Package/tools/AdvanceCalc.java
@@ -0,0 +1,9 @@
+package Package.tools;
+public class AdvanceCalc {
+   public AdvanceCalc() {
+   }
+
+   public int add(int var1, int var2) {
+      return var1 + var2 + 1;
+   }
+}
---
+++ CORE_JAVA/Package/tools/Calc.java
@@ -0,0 +1,11 @@
+package Package.tools;
+public class Calc {
+   public Calc() {
+      System.out.println("In Calc");
+   }
+
+   public int add(int var1, int var2) {
+      return var1 + var2;
+   }
+}
+
