28c0cc13a79877d613db955cd73c3edc543483ca
Ajhar21 <2017ajharakanda@gmail.com>
2025-11-06 02:12:16 +0600

    20251106 Spring completed

--- Exploring Spring Framework/spring1/src/main/java/org/bih/Desktop.java
+++ Exploring Spring Framework/spring1/src/main/java/org/bih/Desktop.java
@@ -1,6 +1,9 @@
 package org.bih;
 
 public class Desktop implements Computer{
+    public Desktop(){
+        System.out.println("Desktop object created");
+    }
     @Override
     public void compile(){
         System.out.println("Compiling by Desktop");
