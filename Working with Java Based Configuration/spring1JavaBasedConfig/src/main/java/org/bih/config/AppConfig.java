2253d701b3819c3eb71db381a8a0de60d7a9866d
Ajharul Islam Akanda <ajhaka@icps.local>
2025-11-08 18:47:38 +0600

    20251108 java based config

--- Working with Java Based Configuration/spring1JavaBasedConfig/src/main/java/org/bih/config/AppConfig.java
+++ Working with Java Based Configuration/spring1JavaBasedConfig/src/main/java/org/bih/config/AppConfig.java
@@ -1,13 +1,30 @@
 package org.bih.config;
 
+import org.bih.Alien;
+import org.bih.Computer;
 import org.bih.Desktop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+//    public Alien alien(@Autowired Computer com){
+    public Alien alien( Computer com){  //new version no need to mention Autowired
+        Alien alien=new Alien();
+        alien.setAge(26);
+//        alien.setCom(desktop());    //tight coupling
+        alien.setCom(com);
+        return alien;
+    }
+
+    @Bean
+//    @Bean(name = "com") //single bean name, better to use it
+//    @Bean(name = {"com1","Weapon","Desktop"})   //multiple bean name
+//    @Scope(value = "prototype")
     public Desktop desktop(){
         return new Desktop();
     }
