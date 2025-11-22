e26c40ed8e25e00908aabb0d51353c1356c27232
Ajharul Islam Akanda <ajhaka@icps.local>
2025-11-22 17:52:18 +0600

    Spring JDBC project completion 20251122

--- Spring JDBC/SpringJDBCProject/src/main/java/com/bih/SpringJDBCProject/repo/StudentRepo.java
+++ Spring JDBC/SpringJDBCProject/src/main/java/com/bih/SpringJDBCProject/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.bih.SpringJDBCProject.repo;
 import com.bih.SpringJDBCProject.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,13 +26,46 @@ public class StudentRepo {
     }
 
     public void save(Student s1) {
-        String sql="insert into student(rollNo,sName,marks) values(?,?,?)";
-        int row=jdbc.update(sql,s1.getRollNo(),s1.getsName(),s1.getMarks());
-        System.out.println(row+" effected");
+        String sql = "insert into student(rollNo,sName,marks) values(?,?,?)";
+        int row = jdbc.update(sql, s1.getRollNo(), s1.getsName(), s1.getMarks());
+        System.out.println(row + " effected");
     }
 
     public List<Student> findAll() {
-        List<Student> stu=new ArrayList<>();
-        return stu;
+
+        String sql = "select * from student";
+        /*
+        RowMapper mapper= new RowMapper() {
+            @Override
+            public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student s=new Student();
+                s.setRollNo(rs.getInt("rollNo"));
+                s.setsName(rs.getString("sName"));
+                s.setMarks(rs.getInt("marks"));
+                return s;
+            }
+        };
+        */
+
+        /*
+        RowMapper mapper= ( rs,  rowNum) -> {
+                Student s=new Student();
+                s.setRollNo(rs.getInt("rollNo"));
+                s.setsName(rs.getString("sName"));
+                s.setMarks(rs.getInt("marks"));
+                return s;
+        };
+
+        return jdbc.query(sql,mapper);
+
+        */
+
+        return jdbc.query(sql,(rs, rowNum) -> {
+            Student s = new Student();
+            s.setRollNo(rs.getInt("rollNo"));
+            s.setsName(rs.getString("sName"));
+            s.setMarks(rs.getInt("marks"));
+            return s;
+        });
     }
 }
